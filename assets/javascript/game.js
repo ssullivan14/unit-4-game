8b168a690df5746e34ef04faade24e784b561d56
sara sullivan <saralsullivan@gmail.com>
2019-08-30 13:41:50 -0400

    functioning correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,105 +3,111 @@
 //how we start the functions in our jquery
 
 //random number generator for container-1
+
+
+//needed to start a js page
 $(document).ready(function () {
+
+    //variable for functions and for loops
+    var counter = 0;
+    var wins = 0;
+    var losses = 0;
+    //global math random variable
+    var random = Math.floor(Math.random() * 120 + 19)
+    var gem1 = Math.floor(Math.random() * 12 + 1)
+    var gem2 = Math.floor(Math.random() * 12 + 1)
+    var gem3 = Math.floor(Math.random() * 12 + 1)
+    var gem4 = Math.floor(Math.random() * 12 + 1)
+
+    //functions to text the wins to the wins div
+    $('#wins').text(wins + "");
+    $('#losses').text(losses + "");
+
     $("#random-button").on("click", function () {
-        var random = Math.floor(Math.random() * 50 + 11);
+        //var random = Math.floor(Math.random() * 50 + 11);
         $("#random-button").text(random);
     });
+
+    //setting up clicks for each gem
+    $('#yellow').on('click', function () {
+        counter = counter + gem1;
+        console.log("counter= " + counter);
+        $('#counter').text(counter);
+        //sets win/lose conditions
+        if (counter == random) {
+            winner();
+            
+        } else if (counter > random) {
+            loser();
+           
+        }
+    });
+
+    $('#purple').on('click', function () {
+        counter = counter + gem2;
+        console.log("counter= " + counter);
+        $('#counter').text(counter);
+        //sets win/lose conditions
+        if (counter == random) {
+            winner();
+        } else if (counter > random) {
+            loser();
+        }
+    });
+
+    $('#red').on('click', function () {
+        counter = counter + gem3;
+        console.log("counter= " + counter);
+        $('#counter').text(counter);
+        //sets win/lose conditions
+        if (counter == random) {
+            winner();
+        } else if (counter > random) {
+            loser();
+        }
+    });
+
+    $('#orange').on('click', function () {
+        counter = counter + gem4;
+        console.log("counter= " + counter);
+        $('#counter').html(counter);
+        //sets win/lose conditions
+        if (counter == random) {
+            winner();
+        } else if (counter > random) {
+            loser();
+        }
+    })
+
+    //add reset the game
+    function reset() {
+        randomNumber = Math.floor(Math.random() * 120 + 19)
+        console.log(random)
+        $('#random-button').text(randomNumber);
+        gem1 = Math.floor(Math.random() * 12 + 1);
+        gem2 = Math.floor(Math.random() * 12 + 1);
+        gem3 = Math.floor(Math.random() * 12 + 1);
+        gem4 = Math.floor(Math.random() * 12 + 1);
+        counter = 0;
+        $('#counter').text(counter);
+    }
+    //alerts for winning and losing
+    function winner() {
+        alert("You Won!");
+        wins++;
+        $('#wins').text(wins);
+        reset();
+    }
+
+    function loser() {
+        alert("You lose!");
+        losses++;
+        $('#losses').text(losses);
+        reset()
+    }
+
+
 });
-
-//creating random number for each gem between 1-12
-
-var counter = 0;
-var wins = 0;
-var losses = 0;
-var Random = Math.floor(Math.random() * 50 + 11)
-var gem1 = Math.floor(Math.random() * 11 + 1)
-var gem2 = Math.floor(Math.random() * 11 + 1)
-var gem3 = Math.floor(Math.random() * 11 + 1)
-var gem4 = Math.floor(Math.random() * 11 + 1)
-
-$('#wins').text(wins + "");
-$('#losses').text(losses + "");
-
-//alerts for winning and losing
-function wins() {
-    alert("You Won!");
-    wins++;
-    $('#wins').text(wins);
-    reset();
-}
-
-function loser() {
-    alert("You lose!");
-    losses++;
-    $('#losses').text(losses);
-    reset()
-}
-//setting up clicks for each gem
-$('#yellow').on('click', function () {
-    counter = counter + gem1;
-    console.log("counter= " + counter);
-    $('#counter').text(counter);
-    //sets win/lose conditions
-    if (counter === Random) {
-        wins();
-    } 
-    else if (counter > Random) {
-        loser();
-    }
-})
-$('#purple').on('click', function () {
-    counter = counter + gem2;
-    console.log("counter= " + counter);
-    $('#counter').html(counter);
-    //sets win/lose conditions
-    if (counter === Random) {
-        wins();
-    } else if (counter > Random) {
-        losses();
-    }
-})
-$('#red').on('click', function () {
-    counter = counter + gem3;
-    console.log("counter= " + counter);
-    $('#counter').html(counter);
-    //sets win/lose conditions
-    if (counter === Random) {
-        wins();
-    } else if (counter > Random) {
-        losses();
-    }
-})
-
-$('#orange').on('click', function () {
-    counter = counter + gem4;
-    console.log("counter= " + counter);
-    $('#counter').html(counter);
-    //sets win/lose conditions
-    if (counter === Random) {
-        wins();
-    } else if (counter > Random) {
-        losses();
-    }
-});
-
-
-//add reset the game
-// function reset() {
-//     Random = Math.floor(Math.random() * 50 + 11)
-//     console.log(Random)
-//     $('#random-button').text(Random);
-//     gem1 = Math.floor(Math.random() * 11 + 1);
-//     gem2 = Math.floor(Math.random() * 11 + 1);
-//     gem3 = Math.floor(Math.random() * 11 + 1);
-//     gem4 = Math.floor(Math.random() * 11 + 1);
-//     userTotal = 0;
-//     $('#').text(totalScore);
-// }
-
-
-
 
 
 //Player will click on any crystal which will add to the score
